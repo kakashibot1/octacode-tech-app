8114f7834594f51e0a621c9169728656d5429384
octavio wina <botkakashi234@gmail.com>
2026-03-14 02:25:09 +0100

    Update lesson_detail_screen.dart

--- lib/screens/lesson_detail_screen.dart
+++ lib/screens/lesson_detail_screen.dart
@@ -3,23 +3,18 @@ import '../models/lesson.dart';
 
 class LessonDetailScreen extends StatelessWidget {
   final Lesson lesson;
+
   LessonDetailScreen({required this.lesson});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text(lesson.title),
-        backgroundColor: Colors.black,
-      ),
-      body: Padding(
+      appBar: AppBar(title: Text(lesson.title)),
+      body: SingleChildScrollView(
         padding: EdgeInsets.all(16),
-        child: SingleChildScrollView(
-          child: Text(
-            lesson.content,
-            style: TextStyle(color:Colors.green,fontSize:16),
-          ),
+        child: SelectableText(
+          lesson.content,
+          style: TextStyle(fontSize: 16, color: Colors.greenAccent),
         ),
       ),
     );
